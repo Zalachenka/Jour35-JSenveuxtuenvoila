1f3c28c0fd8497c1c4c6837785617119d5ecfcf6
zalachenka <saliou.armony@gmail.com>
2019-05-20 13:51:14 +0200

    1bis done

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// var footerEl = document.querySelector("footer");
+// var clickValue = parseInt(footerEl).value
+// 	clickValue = isNaN(clickValue) ? 0 : clickValue;
+
+// var onFooterClick = function() {
+// 	  clickValue++;
+//     document.querySelector("footer").value = clickValue
+// 	console.log("click number " + clickValue);
+
+// };
+// footerEl.addEventListener("click", onFooterClick);
 var footerEl = document.querySelector("footer");
+var clickValue = 0
 var onFooterClick = function() {
-	console.log("clique");
+	clickValue++;
+	console.clear();
+	console.log("click number " + clickValue);
 };
 footerEl.addEventListener("click", onFooterClick);
